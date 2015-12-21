d77f88dde48a22a02855a9ba63d38f61c7107d7f
Mathieu Leplatre <mathieu@mozilla.com>
2015-12-21 16:13:10 +0100

    Back to development: 0.3.0

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ test_requirements = [
 
 setup(
     name='kinto-attachment',
-    version='0.2.0',
+    version='0.3.0.dev0',
     description="Attach files to Kinto records",
     long_description=readme + '\n\n' + history,
     author="Mozilla",
