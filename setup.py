954d42ada0994e95dffb77364c157f37241a11fa
Mathieu Leplatre <mathieu@mozilla.com>
2018-12-19 13:25:53 +0100

    Preparing release 6.0.1

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ test_requirements = [
 
 setup(
     name='kinto-attachment',
-    version='6.1.0.dev0',
+    version='6.0.1',
     description="Attach files to Kinto records",
     long_description=readme + '\n\n' + history,
     author="Mozilla",
